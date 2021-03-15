6737469b6e1f4b44bb887999c44af27956263d57
Osipov Andrey Alekseevich <57837642+osipov-andrey@users.noreply.github.com>
2021-03-15 16:25:20 +0300

    Update find_deps_dpkg.sh

--- find_deps_dpkg.sh
+++ find_deps_dpkg.sh
@@ -5,7 +5,7 @@ set -e
 DEPS=$(find $1 -type f -perm /a+x -o -name '*.so' \
         | xargs -n1 ldd 2>1 \
         | grep "=>" \
-        | awk '{print $3}' \
+        | awk -v dep="$3" '{print dep}' \
         | grep "/" \
         | sort -u)
 
